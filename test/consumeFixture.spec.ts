5a5c48d4d30b35ae4c36c3205faf1d4872aa188f
Abhinav Sharma <777abhi@gmail.com>
2021-05-26 17:22:58 +0100

    rename folder basics

--- test/consumeFixture.spec.ts
+++ test/consumeFixture.spec.ts
@@ -1,6 +1,5 @@
 import {it, expect} from './fixture'
 
-it("get browser path tests on multiple web pages", async ({ chrome }) => {
-    
+it("get browser path tests", async ({ chrome }) => {
   console.log(`${chrome}`);
 });
