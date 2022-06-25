0170a11c9fd216b7cee5dee7b40b1c56df921dfe
Abhinav Sharma <777abhi@gmail.com>
2022-06-25 21:47:31 +0100

    visual testing scenarios

--- test-toptal/new-project-practice/page/LoginPage.ts
+++ test-toptal/new-project-practice/page/LoginPage.ts
@@ -5,18 +5,24 @@ export class LoginPage {
   readonly userNameInput: Locator;
   readonly passwordInput: Locator;
   readonly signInBtn: Locator;
+  readonly loginForm: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.userNameInput = page.locator("#user_login");
     this.passwordInput = page.locator("#user_password");
     this.signInBtn = page.locator('input[value="Sign in"]');
+    this.loginForm = page.locator("#login_form");
   }
 
-  async performLogin(username: string, passowrd: string) {
+  async performLogin(username: string, password: string) {
     await this.userNameInput.type(username);
-    await this.passwordInput.type(passowrd);
+    await this.passwordInput.type(password);
     await this.signInBtn.click();
     await this.page.goBack(); // fix security issue with the test website
+  }
+
+  async snapshotLoginForm() {
+    expect(await this.loginForm.screenshot()).toMatchSnapshot("login-form.png");
   }
 }
