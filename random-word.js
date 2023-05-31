46f0d7591002fc446e5f6ef4716cb99fcb136283
Abhinav Sharma <777abhi@gmail.com>
2023-05-31 23:00:22 +0100

    test: using @actions/core

--- random-word.js
+++ random-word.js
@@ -1,6 +1,7 @@
+const core = require('@actions/core');
+
 const words = ['apple', 'banana', 'orange', 'grape', 'melon', 'pear'];
 const randomIndex = Math.floor(Math.random() * words.length);
 const randomWord = words[randomIndex];
 
-
-module.exports = randomWord;
+core.setOutput('RANDOM_WORD', randomWord);
